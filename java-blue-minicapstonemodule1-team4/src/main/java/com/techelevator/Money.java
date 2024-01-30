8296ac0ae8d1ba0f9177cd04c7e4a032ee2168f6
JonathanAFoy <149273011+JonathanAFoy@users.noreply.github.com>
2024-01-30 11:20:27 -0500

    Testing

---
+++ java-blue-minicapstonemodule1-team4/src/main/java/com/techelevator/Money.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Money extends VendingMachineCLI{
+
+
+
+
+
+}
+
---
+++ java-blue-minicapstonemodule1-team4/src/main/java/com/techelevator/Order.java
@@ -0,0 +1,22 @@
+package com.techelevator;
+
+// Pass in order object to CLI method.
+public class Order {
+
+   // public static List<Item> itemsList = new ArrayList<>();
+
+
+    public void addItem(Item item) {
+        //this.itemsList.add(item);       // Does this belong in item class?
+
+        // Write to log if transaction goes through.
+        // If currentMoneyProvided.comparedTo
+        // Print each item, and add item to log file for future reference?
+    }
+
+
+
+    //public static List<Item> getItems() {
+      //  return itemsList;
+    //}
+}
